e76ad44adc2bc0ba73c2e4a73e6d259fb9aa7f77
叶宏业 <305554601@qq.com>
2018-08-02 14:25:54 +0800

    搭建了App基本框架

---
+++ MaintainDriverClient/MPLeftMenuViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MPLeftMenuViewController.swift
+//  MaintainDriverClient
+//
+//  Created by Maple on 2018/8/2.
+//  Copyright © 2018年 Maple. All rights reserved.
+//
+
+import UIKit
+
+class MPLeftMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.green
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MaintainDriverClient/MPUtils.swift
@@ -0,0 +1,15 @@
+//
+//  MPUtils.swift
+//  MaintainDriverClient
+//
+//  Created by Maple on 2018/8/2.
+//  Copyright © 2018年 Maple. All rights reserved.
+//
+
+import UIKit
+
+/// 工具类
+class MPUtils {
+    static let screenH: CGFloat = UIScreen.main.bounds.height
+    static let screenW: CGFloat = UIScreen.main.bounds.width
+}
